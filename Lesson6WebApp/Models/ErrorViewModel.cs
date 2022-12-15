5244ffe9002f19ddb0d04cd17769c8e3ea093878
mpho.phahla01@outlook.com <mpho.phahla01@outlook.com>
2022-12-15 15:56:10 +0200

    Add project files.

---
+++ Lesson6WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lesson6WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
